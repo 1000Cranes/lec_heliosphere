1293d3509d60e55ceb119117db35e6d0cdb1b618
Loura Coons <loura@mightymeteorites.com>
2015-05-14 01:33:03 +0000

    initial commit

--- client/views/base/startup.js
+++ client/views/base/startup.js
@@ -1,3 +1,7 @@
-Meteor.startup(function () {
-  $(".button-collapse").sideNav();   
+// Meteor.startup(function () {
+//   $(".button-collapse").sideNav();   
+// });
+
+Template.header.onRendered(function(){
+  this.$(".button-collapse").sideNav();
 });
