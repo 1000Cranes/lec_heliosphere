903deb2fd0ed16bd1c25bc34150c7abeb6137658
Loura <loura@mightymeteorites.com>
2015-07-27 20:53:02 +0000

    reply comment get username from profile.name

--- client/autoform.js
+++ client/autoform.js
@@ -35,11 +35,7 @@ AutoForm.hooks({
         doc.createDate = new Date();
        var user = Meteor.user();
         doc.gravatar = Gravatar.imageUrl(user.emails[0].address);
-        if(user.userName === null || user.userName === undefined || user.userName.match(/^ *$/) !== null){
-          doc.userName = user.emails[0].address.substring(0,3); // No username so return first three characters
-        } else {
-          doc.userName = userName; 
-        }
+        doc.userName = user.profile.name; 
         return doc;
        }
      }
